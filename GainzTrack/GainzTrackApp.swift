0aebbda1ba2f65ac0ccff7a9d1c436836fda1187
Quinterock <luisgqr1975@gmail.com>
2025-08-19 14:53:25 -0600

    First Commit

---
+++ GainzTrack/GainzTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainzTrackApp.swift
+//  GainzTrack
+//
+//  Created by Luis Quintero on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct GainzTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GainzTrackTests/GainzTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  GainzTrackTests.swift
+//  GainzTrackTests
+//
+//  Created by Luis Quintero on 19/08/25.
+//
+
+import Testing
+@testable import GainzTrack
+
+struct GainzTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
