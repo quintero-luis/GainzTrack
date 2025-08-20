54ae4c191a1b5dd4c15e9ea7a54c318e3dc632ed
Quinterock <luisgqr1975@gmail.com>
2025-08-20 17:51:36 -0600

    MuscleGroupRepository added

---
+++ GainzTrack/Data/Repositories/MuscleGroupRepository.swift
@@ -0,0 +1,44 @@
+//
+//  MuscleGroupRepository.swift
+//  GainzTrack
+//
+//  Created by Luis Quintero on 20/08/25.
+//
+
+import Foundation
+import SwiftData
+
+final class MuscleGroupRepository: MuscleGroupRepositoryProtocol {
+    private let context: ModelContext
+    
+    init(context: ModelContext) {
+        self.context = context
+    }
+    // TODO:
+    /*
+     func getAllMuscleGroups(for day: Day) async throws -> [MuscleGroup] {
+         let descriptor = FetchDescriptor<MuscleGroup>(
+             predicate: #Predicate { $0.day == day } // si agregas la relación inversa day en MuscleGroup
+         )
+         return try context.fetch(descriptor)
+     }
+     */
+    
+    func getAllMuscleGroups(for day: Day) async throws -> [MuscleGroup] {
+        return day.muscleGroups
+    }
+    
+    func addMuscleGroup(_ muscleGroup: MuscleGroup, to day: Day) async throws {
+        day.muscleGroups.append(muscleGroup)
+        try context.save()
+    }
+    
+    func updateMuscleGroup(_ muscleGroup: MuscleGroup) async throws {
+        try context.save()
+    }
+    
+    func deleteMuscleGroup(_ muscleGroup: MuscleGroup) async throws {
+        context.delete(muscleGroup)
+        try context.save()
+    }
+}
