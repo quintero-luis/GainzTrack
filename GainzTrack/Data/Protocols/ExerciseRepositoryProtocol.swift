f0e78ec7602cf46eef710de5f355511287a2f65d
Quinterock <luisgqr1975@gmail.com>
2025-08-20 18:26:07 -0600

    ExerciseRepositoryProtocol added

---
+++ GainzTrack/Data/Protocols/ExerciseRepositoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ExerciseRepositoryProtocol.swift
+//  GainzTrack
+//
+//  Created by Luis Quintero on 20/08/25.
+//
+
+import Foundation
+
+protocol ExerciseRepositoryProtocol {
+    func getAllExercises(for muscleGroup: MuscleGroup) async throws -> [Exercise]
+    func addExercise(_ exercise: Exercise, to muscleGroup: MuscleGroup) async throws
+    func updateExercise(_ exercise: Exercise) async throws
+    func deleteExercise(_ exercise: Exercise) async throws
+}
