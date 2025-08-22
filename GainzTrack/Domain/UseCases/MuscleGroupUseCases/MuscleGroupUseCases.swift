36de77d194ca26baff1403aa9e40045bcd0d1ddc
Quinterock <luisgqr1975@gmail.com>
2025-08-22 15:31:23 -0600

    Only one MuscleGroupUseCases w Protocol

---
+++ GainzTrack/Domain/UseCases/MuscleGroupUseCases/MuscleGroupUseCases.swift
@@ -0,0 +1,44 @@
+//
+//  AddMuscleGroupUseCase.swift
+//  GainzTrack
+//
+//  Created by Luis Quintero on 20/08/25.
+//
+
+import Foundation
+
+protocol MuscleGroupUseCasesProtocol {
+    func fetchAllMuscleGroups() async throws -> [MuscleGroup]
+    func fetchMuscleGroup(by id: UUID) async throws -> MuscleGroup?
+    func createMuscleGroup(_ muscleGroup: MuscleGroup) async throws
+    func updateMuscleGroup(_ muscleGroup: MuscleGroup) async throws
+    func deleteMuscleGroup(_ muscleGroup: MuscleGroup) async throws
+}
+
+final class MuscleGroupUseCases: MuscleGroupUseCasesProtocol {
+    private let repository: MuscleGroupRepositoryProtocol
+
+    init(repository: MuscleGroupRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func fetchAllMuscleGroups() async throws -> [MuscleGroup] {
+        try await repository.getAllMuscleGroups()
+    }
+
+    func fetchMuscleGroup(by id: UUID) async throws -> MuscleGroup? {
+        try await repository.getMuscleGroup(by: id)
+    }
+
+    func createMuscleGroup(_ muscleGroup: MuscleGroup) async throws {
+        try await repository.addMuscleGroup(muscleGroup)
+    }
+
+    func updateMuscleGroup(_ muscleGroup: MuscleGroup) async throws {
+        try await repository.updateMuscleGroup(muscleGroup)
+    }
+
+    func deleteMuscleGroup(_ muscleGroup: MuscleGroup) async throws {
+        try await repository.deleteMuscleGroup(muscleGroup)
+    }
+}
