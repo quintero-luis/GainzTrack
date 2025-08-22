f8c1606aa2f1e94b97305a8c5cd63dfec87e489a
Quinterock <luisgqr1975@gmail.com>
2025-08-22 15:28:45 -0600

    Only one DayUseCases w Protocol

---
+++ GainzTrack/Domain/UseCases/DayUseCases/DayUseCases.swift
@@ -0,0 +1,46 @@
+//
+//  AddDayUseCase.swift
+//  GainzTrack
+//
+//  Created by Luis Quintero on 20/08/25.
+//
+
+import Foundation
+
+protocol DayUseCasesProtocol {
+    func fetchAllDays() async throws -> [Day]
+    func fetchDay(by date: Date) async throws -> Day?
+    func createDay(_ day: Day) async throws
+    func updateDay(_ day: Day) async throws
+    func deleteDay(_ day: Day) async throws
+}
+
+final class DayUseCases: DayUseCasesProtocol {
+    private let repository: DayRepositoryProtocol
+
+    init(repository: DayRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func fetchAllDays() async throws -> [Day] {
+        try await repository.getAllDays()
+    }
+
+    func fetchDay(by date: Date) async throws -> Day? {
+        try await repository.getDay(by: date)
+    }
+
+    func createDay(_ day: Day) async throws {
+        try await repository.addDay(day)
+    }
+
+    func updateDay(_ day: Day) async throws {
+        try await repository.updateDay(day)
+    }
+
+    func deleteDay(_ day: Day) async throws {
+        try await repository.deleteDay(day)
+    }
+}
+
+
