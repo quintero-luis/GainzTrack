07f3e263a740a5562b2b50fa1ab53f572fd26a20
Quinterock <luisgqr1975@gmail.com>
2025-08-28 11:54:09 -0600

    Set ExerciseListV to gray

--- GainzTrack/Presentation/Views/ExerciseListView.swift
+++ GainzTrack/Presentation/Views/ExerciseListView.swift
@@ -18,41 +18,51 @@ struct ExerciseListView: View {
     
     var body: some View {
         NavigationStack {
-            Text(selectedMuscleGroup.name)
-            Spacer()
-            if selectedMuscleGroup.exercises.isEmpty {
-                Text("This muscleGroup has no exercises yet")
-                    .foregroundColor(.gray)
-            } else {
-                List(selectedMuscleGroup.exercises, id: \.id) { exercise in
-                    NavigationLink {
-                        // Todo navigation to ExerciseSet for selected Exercise
-                    } label: {
-                        Text(exercise.name)
-                    }
+            ZStack {
+                Color(.systemGray6) // Set all view to gray
+                    .ignoresSafeArea()
+                VStack {
+                    Text(selectedMuscleGroup.name)
+                    Spacer()
+                    if selectedMuscleGroup.exercises.isEmpty {
+                        Text("This muscleGroup has no exercises yet")
+                            .foregroundColor(.gray)
+                    } else {
+                        List(selectedMuscleGroup.exercises, id: \.id) { exercise in
+                            NavigationLink {
+                                // Todo navigation to ExerciseSet for selected Exercise
+                            } label: {
+                                Text(exercise.name)
+                            }
+                        }
+                    } // else
                 }
-            } // else
-            Spacer()
-            HStack {
-                Spacer()
                 
-                // Button to add exercise to the selected muscle group
-                NavigationLink {
-                    AddExerciseView(
-                        muscleGroupVM: musclegroupVM,
-                        exerciseVM: exerciseVM,
-                        selectedMuscleGroup: selectedMuscleGroup
-                    )
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: 32))
+                VStack { // VStack Floating Button
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        
+                        // MARK: PLUS Button to add exercise to the selected muscle group
+                        NavigationLink {
+                            AddExerciseView(
+                                muscleGroupVM: musclegroupVM,
+                                exerciseVM: exerciseVM,
+                                selectedMuscleGroup: selectedMuscleGroup
+                            )
+                        } label: {
+                            Image(systemName: "plus")
+                                .font(.system(size: 32))
+                                .padding()
+                                .foregroundColor(.black)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .shadow(radius: 4, x: 0, y: 2)
+                        }
                         .padding()
-                        .foregroundColor(.black)
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(radius: 4, x: 0, y: 2)
-                }
-                .padding()
+                    }
+                } // VStack Floating Button
+
             }
         } // Navigation Stack
         .task {
